1e8eb0158fc6c0d1b263e75130968c3da4bc0429
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2022-12-13 01:34:03 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<title>GitOps - Flux - Demo</title>
+
+<body style="background: darkblue;">
+
+<?php
+echo "<div style='height: 100vh;
+			display: flex;
+            justify-content: center;
+            align-items: center;'>
+
+		<p style='font-size: 72px; 
+		          color: white;
+				  font-weight: bold;'>
+			pod name : " . getenv("MY_POD_NAME") . "<br> 
+			pod ip   : " . getenv("MY_POD_IP") . " <br> 
+			version  : 1.0.0  
+		</p>";
+echo "</div>";
+?>
+</body>
+</html>
