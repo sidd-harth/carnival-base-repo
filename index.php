083c922713faa33b40eab68aac4a6839399274b1
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2022-12-13 16:15:09 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,38 @@
 <html>
 <title>GitOps - Flux - Demo</title>
 
-<body style="background: red;">
+<body style="background: darkblue;">
 
 <?php
 echo "<div style='height: 100vh;
-	          display: flex;
-                  justify-content: center;
-                  align-items: center;'>
+			display: flex;
+            justify-content: center;
+            align-items: center;'>
 
 		<p style='font-size: 72px; 
 		          color: white;
-		  	  font-weight: bold;'>
+				  font-weight: bold;'>
 			pod name : " . getenv("MY_POD_NAME") . "<br> 
 			pod ip   : " . getenv("MY_POD_IP") . " <br> 
-			version  : 9.1.0  
-		</p>";
-echo "</div>";
+			version  : 11.0.0  
+		</p>
+	  </div>
+	  <div id='presentDate' 
+           style='font-size: 72px; 
+		          color: white;
+			      font-weight: bold;display: flex;
+				  justify-content: center;
+                  align-items: center;'>";       
+echo  "</div>";
 ?>
 </body>
+<script>
+	function getDate() {
+	let presentDate = document.getElementById('presentDate');
+	let present = new Date();
+	presentDate.innerHTML = `${present}`;
+	}
+
+	setInterval(getDate, 1000);
+</script>
 </html>
